4a143dee17373462b932f68042b568ddc86a91b3
Inessa Makarchuk <inessa.makarchuk@gmail.com>
2019-07-08 00:42:11 +0300

    Вернула методы isElementPresent и isAlertPresent.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class BaseHelper {
@@ -24,5 +26,23 @@ public class BaseHelper {
       }
     }
 
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
   }
 }
