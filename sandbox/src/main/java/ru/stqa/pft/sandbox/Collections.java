df80d42ff900f97d847a202a0770e439add35768
Inessa Makarchuk <inessa.makarchuk@gmail.com>
2019-07-15 19:06:07 +0300

    Коллекции: инициализация массива элементов разными способами

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,34 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+  public static void main(String[] args) {
+   /* String[] langs = new String[4];
+   * langs[0] = "Java";
+   * langs[1] = "C#";
+   * langs[2] = "Python";
+   * langs[3] = "PHP";
+   * */
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    for (String l : langs) {
+      System.out.println("Я хочу выучить " + l);
+    }
+
+  /*
+    List<String> languages = new ArrayList<String>;
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+    languages.add("PHP");
+*/
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+    for (String l : languages) {
+      System.out.println("Я хочу выучить " + l);
+    }
+  }
+}
