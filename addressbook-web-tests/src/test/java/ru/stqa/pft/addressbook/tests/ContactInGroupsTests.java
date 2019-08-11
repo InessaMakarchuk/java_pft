ab6146cdb36b374aa98627d013573ff28a650c88
Inessa Makarchuk <inessa.makarchuk@gmail.com>
2019-08-11 04:06:34 +0300

    Реализованы проверки через БД в тестах на добавление контактов в группу и удаление контактов из группы.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInGroupsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInGroupsTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
@@ -8,8 +7,8 @@ import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 import java.io.File;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class ContactInGroupsTests extends TestBase {
@@ -36,6 +35,57 @@ public class ContactInGroupsTests extends TestBase {
     Groups groups = app.db().groups();
     for (ContactData contact : contacts) {
       if (contact.getGroups().size() != groups.size()) {
+        Groups groupsBefore = contact.getGroups();
+        app.contact().selectContactById(contact.getId());
+        app.contact().selectGroup(contact);
+        app.contact().addToSelectedCroups();
+        app.contact().showInSelectedGroup();
+        Contacts updatedContacts = app.db().contacts();
+        for (ContactData updatedContact : updatedContacts) {
+          if (updatedContact.getId() == contact.getId()) {
+            Groups groupsAfter = updatedContact.getGroups();
+            assertThat(groupsAfter.size(), equalTo(groupsBefore.size() + 1));
+            updatedContact.getGroups().removeAll(groupsBefore);
+            assertThat(groupsAfter, equalTo(groupsBefore.withAdded(updatedContact.getGroups().iterator().next())));
+          }
+        }
+        app.contact().showAllGroups();
+      }
+    }
+  }
+
+
+  @Test
+  public void testRemoveContactFromGroup() {
+    Contacts contacts = app.db().contacts();
+    for (ContactData contact : contacts) {
+      if (contact.getGroups().size() != 0) {
+        Groups groupsBefore = contact.getGroups();
+        app.contact().filterByGroup(contact);
+        app.contact().selectContactById(contact.getId());
+        app.contact().removeFromSelectedGroups();
+        app.contact().showInSelectedGroup();
+        Contacts updatedContacts = app.db().contacts();
+        for (ContactData updatedContact : updatedContacts) {
+          if (updatedContact.getId() == contact.getId()) {
+            Groups groupsAfter = updatedContact.getGroups();
+            assertThat(groupsAfter.size(), equalTo(groupsBefore.size() - 1));
+            groupsBefore.removeAll(groupsAfter);
+            assertThat(groupsAfter, equalTo(contact.getGroups().without(groupsBefore.iterator().next())));
+          }
+        }
+        app.contact().showAllGroups();
+      }
+    }
+  }
+
+  /* Проверка для UI, нужно чистить кэш при добавлении контакта в группу, требуется доработать
+  @Test
+  public void testAddContactToGroupVerifyInUI() {
+    Contacts contacts = app.db().contacts();
+    Groups groups = app.db().groups();
+    for (ContactData contact : contacts) {
+      if (contact.getGroups().size() != groups.size()) {
         app.contact().selectContactById(contact.getId());
         app.contact().selectGroup(contact);
         app.contact().addToSelectedCroups();
@@ -57,32 +107,10 @@ public class ContactInGroupsTests extends TestBase {
       }
     }
   }
+  */
 
-
-  @Test
-  public void testRemoveContactFromGroup() {
-    Contacts contacts = app.db().contacts();
-    for (ContactData contact : contacts) {
-      if (contact.getGroups().size() != 0) {
-        app.contact().filterByGroup(contact);
-        app.contact().selectContactById(contact.getId());
-        app.contact().removeFromSelectedGroups();
-        app.contact().showInSelectedGroup();
-        Contacts contactsInGroup = app.contact().all();
-        Contacts thisContact = new Contacts(contactsInGroup.stream()
-                .map((g) -> new ContactData().withId(contact.getId()).withFirstName(contact.getFirstName())
-                        .withLastName(contact.getLastName()).withAddress(contact.getAddress())
-                        .withAllPhones(Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
-                                .stream().filter((s) -> !s.equals(""))
-                                .collect(Collectors.joining("\n")))
-                        .withAllEmailAddresses(Arrays.asList(contact.getEmailAddress(), contact.getEmailAddress2(), contact.getEmailAddress3())
-                                .stream().filter((s) -> !s.equals(""))
-                                .map(ContactInformationTests::cleaned)
-                                .collect(Collectors.joining("\n"))))
-                .collect(Collectors.toList()));
-        Assert.assertFalse(contactsInGroup.containsAll(thisContact));
-        app.contact().showAllGroups();
-      }
-    }
-  }
 }
+
+
+
+
