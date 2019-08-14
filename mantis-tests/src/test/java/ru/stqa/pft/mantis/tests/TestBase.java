f140d0611a3ef50527c22215d4121fa488a42a73
Inessa Makarchuk <inessa.makarchuk@gmail.com>
2019-08-14 19:16:52 +0300

    Реализовано получение почты на встроенный в тесты почтовый сервер. Создан тест на регистрацию пользователя. Реализовано протоколирования запуска тестов.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,13 +1,21 @@
 package ru.stqa.pft.mantis.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   public static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -21,6 +29,16 @@ public class TestBase {
   public void tearDown() throws IOException {
     app.ftp().restore("config_inc.php.bak", "config_inc.php");
     app.stop();
+  }
+
+  @BeforeMethod
+  public void LogTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + "with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void LogTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
   }
 
 }
