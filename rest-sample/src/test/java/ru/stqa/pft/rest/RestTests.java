6c5f0dc1e86617a16683674515ad0f4ab79f8071
Inessa Makarchuk <inessa.makarchuk@gmail.com>
2019-08-22 20:21:35 +0300

    Добавлен тест, проверяющий наличие неисправленных багов в Bugify и при их наличии тест игнорируется.

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -1,19 +1,11 @@
 package ru.stqa.pft.rest;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.Test;
 import java.io.IOException;
 import java.util.Set;
-
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestTests extends TestBase {
 
   @Test
   public void testCreateIssue() throws IOException {
@@ -25,24 +17,13 @@ public class RestTests {
     assertEquals(newIssues, oldIssues);
   }
 
-  private Set<Issue> getIssues() throws IOException {
-    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json?limit=2000"))
-            .returnContent().asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    JsonElement issues = parsed.getAsJsonObject().get("issues");
-    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());
-  }
-
-  private Executor getExecutor() {
-    return Executor.newInstance().auth("3b1e5c22251e2bcefcb17b13262abf7f", "");
-  }
-
-  private int createIssue(Issue newIssue) throws IOException {
-    String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json?limit=2000")
-            .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                      new BasicNameValuePair("description", newIssue.getDescription())))
-            .returnContent().asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  @Test
+  public void testSkipIfNotFixedIssuesExist() throws IOException {
+    Set<Issue> issues = getIssues();
+    for (Issue issue : issues) {
+      System.out.println("Issue id: " + issue.getId());
+      System.out.println("Issue status: "+ issue.getState());
+      skipIfNotFixed(issue.getId());
+    }
   }
 }
