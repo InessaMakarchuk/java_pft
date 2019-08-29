33d104f87825e4b4a2a7cd1324efef40b312ccfb
Inessa Makarchuk <inessa.makarchuk@gmail.com>
2019-08-30 02:38:30 +0300

    Добавлен плагин для генерации cucumber.json

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/bdd/GroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/bdd/GroupTests.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.bdd;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "classpath:bdd", plugin = {"pretty", "html:build/cucumber-report"})
+@CucumberOptions(features = "classpath:bdd", plugin = {"pretty", "html:build/cucumber-report",
+        "json:build/cucumber-report/cucumber.json"})
 public class GroupTests extends AbstractTestNGCucumberTests {
 }
